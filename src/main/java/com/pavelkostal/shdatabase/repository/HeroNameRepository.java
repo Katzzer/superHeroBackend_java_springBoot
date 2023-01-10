ceda08a0bed9ab85dc1c5da6c3859e7832a31358
Pavel Kostal <kostal@quantretail.com>
2023-01-10 16:36:27 +0100

    adding url of frontend app for CORS

--- src/main/java/com/pavelkostal/shdatabase/repository/HeroNameRepository.java
+++ src/main/java/com/pavelkostal/shdatabase/repository/HeroNameRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin({"http://localhost:4200", "http://localhost:8081", "https://pavelkostal-superheroapi.herokuapp.com"})
+@CrossOrigin({"http://localhost:4200", "http://localhost:8081", "https://pavelkostal-superheroapi.herokuapp.com", "http://superheroapi.pavelkostal.com"})
 @RepositoryRestResource(collectionResourceRel = "heroName", path="hero-name-list")
 //@Repository
 public interface HeroNameRepository extends JpaRepository  <HeroName, Long> {
